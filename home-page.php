157c56f493bf148f82e5e2e024230e8d4ac1fed1
Marcelo Aguila <web@croop.cl>
2015-03-08 20:53:36 -0300

    homepage

--- home-page.php
+++ home-page.php
@@ -6,15 +6,29 @@ Template Name: Inicio
 
 <?php get_header(); ?>
 
+<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+<div class="row">
+      <div class="large-2 columns">
+              <div class="rss">
+                <a href="https://www.facebook.com/icebergsie"><i class="fa fa-facebook"></i></a>
+                <a href="http://www.twitter.com/IcebergsIE"><i class="fa fa-twitter"></i></a>
+                <a href="https://www.youtube.com/channel/UC9tZBOaI8tNFMTyHo1mFY6A"><i class="fa fa-youtube"></i></a>
+              </div>
+      </div>
+      <div class="large-3 column quees">
+        <a href="" data-reveal-id="iceberg"><i class="fa fa-question-circle"></i> ¿Que es ICEBERG?</a>
+      </div>
+</div>
+
  <div class="inside">
          <div class="row">
            <div class="large-6 columns large-centered">
               <img src="<?php bloginfo('template_directory'); ?>/images/logo.png" alt="Iceberg IE" class="logo">
               <hr>
               <div class="htcontainer">
-              <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+
                 <div class="hometext"><?php the_excerpt(); ?></div>
-              <?php endwhile; endif; ?>
+              
               </div>
            </div>
          </div>
@@ -22,14 +36,57 @@ Template Name: Inicio
           <div id="noticias">
             <div class="row">
               <ul class="large-block-grid-4">
-                <li><img src="http://fakeimg.pl/700x400/00CED1/FFF/?text=img+placeholder"></li>
-                <li><img src="http://fakeimg.pl/700x400/00CED1/FFF/?text=img+placeholder"></li>
-                <li><img src="http://fakeimg.pl/700x400/00CED1/FFF/?text=img+placeholder"></li>
-                <li><img src="http://fakeimg.pl/700x400/00CED1/FFF/?text=img+placeholder"></li>
+                <?php query_posts('orderby=date&order=desc&category_name=noticias&showposts=4'); ?>
+                  <?php while (have_posts()) : the_post(); ?>
+                      <li class="noticia">
+                        <a href="<?php the_permalink();?>">
+                          <?php the_post_thumbnail("slide"); ?>
+                          <h2><?php the_title();?></h1>
+                        </a>
+                      </li>
+                  <?php endwhile;?> 
+                <?php wp_reset_query(); ?>
+
+               <!--  <li class="noticia">
+                  <a href="page.html">
+                  <img src="http://lorempixel.com/1300/800/people">
+                  <h2>Emprendimiento Femenino, aún falta para celebrar</h1>
+                  </a>
+                </li>
+                <li class="noticia">
+                  <a href="page.html">
+                  <img src="http://lorempixel.com/1300/800/people">
+                  <h2>Emprendimiento Femenino, aún falta para celebrar</h1>
+                  </a>
+                </li>
+                <li class="noticia">
+                  <a href="page.html">
+                  <img src="http://lorempixel.com/1300/800/people">
+                  <h2>Emprendimiento Femenino, aún falta para celebrar</h1>
+                  </a>
+                </li>
+                <li class="noticia">
+                  <a href="page.html">
+                  <img src="http://lorempixel.com/1300/800/people">
+                  <h2>Emprendimiento Femenino, aún falta para celebrar</h1>
+                  </a>
+                </li>             -->  
               </ul>
             </div>
           </div>
     </div>
 
+<!-- Modal -->
+
+<div id="iceberg" class="reveal-modal large" data-reveal>
+
+<h1><?php the_title();?></h1>
+
+<?php the_content()?>
+<?php endwhile; endif; ?>
+  <a class="close-reveal-modal">&#215;</a>
+</div>
+
+<!-- /Modal -->
 
 <?php get_footer(); ?>
