b213cd1b06cb28ee35251e6e8916df11d4e35c6d
Marcelo Aguila <web@croop.cl>
2015-03-08 21:05:52 -0300

    homepage

--- single.php
+++ single.php
@@ -22,16 +22,18 @@
     <h1><?php the_title();?></h1>
     <hr>
 
-<?php the_content(); ?>
+	<?php the_content(); ?>
+	<hr>
+	<?php comments_template(); ?>
 
-<?php comments_template(); ?>
-
+<?php endwhile; endif; ?>
 </div>
 
 </div>
 
-   <script src="bower_components/jquery/dist/jquery.min.js"></script>
+    <script src="bower_components/jquery/dist/jquery.min.js"></script>
     <script src="bower_components/foundation/js/foundation.min.js"></script>
     <script src="js/app.js"></script>
   </body>
+
 </html>
