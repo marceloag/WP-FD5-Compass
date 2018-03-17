192d7e75fe17ec1972eae236eb4bd7eac7dee848
Marcelo Aguila <web@croop.cl>
2018-03-17 10:05:55 -0300

    2018 corrections

--- page.php
+++ page.php
@@ -2,10 +2,11 @@
 
   <section id="main">
     <div class="row page">
-      <div class="large-10 large-centered columns">
-					<?php the_content(); ?>
-				<?php endwhile; endif; ?>
-      </div>
+			<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+	      <div class="large-10 large-centered columns">
+						<?php the_content(); ?>
+	      </div>
+			<?php endwhile; endif; ?>
     </div>
   </section>
 
